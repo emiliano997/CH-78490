16c52a9fd9a7994df447f7b923a1c7644ce92d77
emilianoperez <emi.perez997@gmail.com>
2025-10-22 22:25:45 -0300

    [ADD] Clase 6

--- clase_6/src/app/featured/dashboard/courses/courses-module.ts
+++ clase_6/src/app/featured/dashboard/courses/courses-module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing-module';
 import { Courses } from './courses';
-
+import { CoursesTable } from './courses-table/courses-table';
+import { CoursesForm } from './courses-form/courses-form';
+import { SharedModule } from '../../../shared/shared-module';
 
 @NgModule({
-  declarations: [
-    Courses
-  ],
-  imports: [
-    CommonModule,
-    CoursesRoutingModule
-  ]
+  declarations: [Courses, CoursesTable, CoursesForm],
+  imports: [CommonModule, CoursesRoutingModule, SharedModule],
 })
-export class CoursesModule { }
+export class CoursesModule {}
